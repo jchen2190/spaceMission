013ce54d81c51958cfbb99abed82649ba6d0efb7
Jon Chen <jchen2190@gmail.com>
2023-04-19 10:05:11 -0400

    working website

--- space-mission/src/hooks/useAxios.js
+++ space-mission/src/hooks/useAxios.js
@@ -1,15 +1,10 @@
 import { useState, useEffect } from "react";
-// `npm install axios` before starting the server
 import axios from "axios";
 
 function useAxios() {
-    // This holds the result of an API call
   const [data, setData] = useState(null);
-//   This holds the URL that's sent from a component
   const [url, setUrl] = useState("");
-//   This holds an error status based on the results of an axios search
   const [error, setError] = useState(null);
-//   This holds the loading status that's sent from a component
   const [loading, setLoading] = useState(false);
 
   async function customFetch() {
@@ -32,7 +27,6 @@ function useAxios() {
     }
   }
 
-//   When we use setLoading from another component, this useEffect sees the change in state. If loading is true, then it performs the customFetch function. Else, nothing happens.
   useEffect(() => {
     if (loading) {
       customFetch(url);
